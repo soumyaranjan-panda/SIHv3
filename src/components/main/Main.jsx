2cc435ec291f3a48eb70a1e6d7450d4e60798021
Soumrnjn <soumyaranjanpanda988@gmail.com>
2023-09-21 20:00:00 +0530

    updated data.json

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -3,11 +3,13 @@ import Map from "../Map";
 
 const Main = () => {
   return (
-    <div className="bg-blue-300 relative ">
+    <div className="bg-white relative ">
       <div className="flex flex-col justify-center items-center">
-        <div>Map Section</div>
+        <div className=" text-2xl">Map Section</div>
         <div className="m-[15px] p-4 flex flex-row justify-evenly gap-32">
-          <div className="h-[60vh] w-[40vw] bg-p2 "></div>
+          <div className="h-[60vh] w-[40vw]">
+            <img src="accident.png" alt="" />
+          </div>
           <div className="flex flex-row gap-5">
             <div className="h-[60vh] w-[20vw] rounded-md bg-p1 flex flex-row justify-center">
               <h1>dfdfd</h1>

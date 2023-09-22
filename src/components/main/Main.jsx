e407b9d3a9653ec274bab84b60179b7afc2a5786
Soumrnjn <soumyaranjanpanda988@gmail.com>
2023-09-22 19:25:33 +0530

    updated map

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -21,7 +21,6 @@ const Main = () => {
                 <div className="p-6">
                   <div className="flex flex-col gap-2 items-center">
                     <GaugeComponent
-                    
                       arc={{
                         subArcs: [
                           {
@@ -47,18 +46,17 @@ const Main = () => {
                         ],
                       }}
                       value={30}
-                      style={{width: 200}}
+                      style={{ width: 200 }}
                     />
                     <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-500 dark:text-gray-500">
                       Accident Intensity
                     </h5>
                   </div>
-                  
 
                   <button
-                  onClick={()=>{
-                    setShowPara(true)
-                  }}
+                    onClick={() => {
+                      setShowPara(true);
+                    }}
                     type="button"
                     className=" mt-4 inline-block rounded bg-primary px-3 pb-1 pt-1.5 text-xs font-medium uppercase leading-normal text-gray-800 border-2 border-gray-400 shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
                   >
@@ -67,11 +65,17 @@ const Main = () => {
                 </div>
                 <div className="border-t-2 border-neutral-100 px-6 py-3 dark:border-neutral-600 dark:text-gray-800">
                   <h1 className=" font-semibold text-xl">Address</h1>
-                  <p className=" w-56 mx-auto">{showPara? 'Kalinga Nagar, Ghatikia, Bhubaneswar, Odisha 751003': ''}</p>
+                  <p className=" w-56 mx-auto">
+                    {showPara
+                      ? "Kalinga Nagar, Ghatikia, Bhubaneswar, Odisha 751003"
+                      : ""}
+                  </p>
                 </div>
               </div>
             </div>
-            <Map></Map>
+            <div className="h-[60vh] w-[20vw] rounded-md">
+              <Map></Map>
+            </div>
           </div>
         </div>
       </div>

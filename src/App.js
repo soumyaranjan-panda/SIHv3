536bb1c4d4fd9547a680819934b5385b5dbe1846
SoumyaPanda <soumyaranjanpanda988@gmail.com>
2024-11-06 01:39:51 +0530

    Add all changes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import { Toaster } from "react-hot-toast";
-import Home from "./components/home/Home";
-import Navbar from "./components/Navbar/Navbar";
-import Footerr from "./components/footer/Footerr";
-import Main from "./components/main/Main"
-import Features from "./components/features/Features";
-import Rocket1 from './components/utilities/rocket/Rocket1'
-import Testimonials3 from "./components/testimonials/Testimonials3";
-import PreLoader from "./components/preloader/PreLoader";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Footerr from "./components/Footerr";
+import Main from "./components/Main";
+import Features from "./components/Features";
+import Rocket1 from "./components/utilities/rocket/Rocket1";
+import Testimonials3 from "./components/Testimonials3";
 
 function App() {
-  return (
-    <div className="font-san">
-      <PreLoader></PreLoader>
-      <Toaster />
-      <Navbar />
-      <Home />
-      <Features/>
-      <Main/>
-      <Testimonials3/>
-      <Rocket1/>
-      <Footerr />
-    </div>
-  );
+    return (
+        <div className="font-san">
+            <Navbar />
+            <Home />
+            <Features />
+            <Main />
+            <Testimonials3 />
+            <Rocket1 />
+            <Footerr />
+        </div>
+    );
 }
 
 export default App;
